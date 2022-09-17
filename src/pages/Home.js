e53c2e37ea9c65b1b91a1992e9150b670d30fe60
cescofors75 <cescofors75@gmail.com>
2022-09-17 13:37:31 +0200

    exchange

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { supabase } from "../supabase/client"
 import {useNavigate} from 'react-router-dom'
 import TaskList from "../components/TaskList"
 import Search from "../components/Search"
-import { Currency } from "../components/Currency"
+import { Exchange } from "../components/Exchange"
 
 function Home () {
     const navigate = useNavigate()
@@ -18,7 +18,7 @@ function Home () {
       <h1>OemApi</h1>
       <Search/>
       <TaskList />
-      <Currency/>
+      <Exchange/>
         <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
     </div>
     
